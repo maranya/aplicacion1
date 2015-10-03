e7c2d289de3cc60299a368b98b754b00eba841da
maranya <miguelaranya@gmail.com>
2015-10-02 21:24:30 -0500

    enviar cambios 2.

--- app/src/main/java/com/example/bitsolutions/myapplication/activities/MainActivity.java
+++ app/src/main/java/com/example/bitsolutions/myapplication/activities/MainActivity.java
@@ -1,9 +1,11 @@
-package com.example.bitsolutions.myapplication;
+package com.example.bitsolutions.myapplication.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.example.bitsolutions.myapplication.R;
 
 public class MainActivity extends AppCompatActivity {
 
